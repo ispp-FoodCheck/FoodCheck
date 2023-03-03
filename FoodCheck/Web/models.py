5e85f627c53982adccdb20179b155f100f5c43ac
Pedro Luis Soto Santos <pepoluis712@gmail.com>
2023-03-03 13:07:05 +0100

    Base del proyecto
    Creada la base del proyecto con el modelo conceptual creado

---
+++ FoodCheck/Web/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+from django.core.validators import URLValidator
+
+class Dieta(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Alergeno(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
+class Supermercado(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    foto = models.URLField(validators=[URLValidator()])
+
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    imagen = models.URLField(validators=[URLValidator()])
+    #precio = models.FloatField()
+    ingredientes = models.CharField(max_length=200)
+    marca = models.CharField(max_length=50)
+    supermercados = models.ManyToManyField(Supermercado)
+    alergenos = models.ManyToManyField(Alergeno, blank=True)
+    dietas = models.ManyToManyField(Dieta)
+
+    def __str__(self):
+        return self.nombre + ' - ' + self.marca
+    
+class Usuario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    usuario = models.CharField(max_length=50)
+    contraseña = models.CharField(max_length=50)
+    recetaDiaria = models.BooleanField()
+    premiumHasta = models.DateField()   
+    alergenos = models.ManyToManyField(Alergeno, blank=True)
+    dieta = models.ForeignKey(Dieta, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.nombre + ' - ' + self.usuario
+
+class ListaCompra(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto)
+
+    def __str__(self):
+        return "Lista de la compra: "+self.nombre + ' - ' + self.usuario.nombre
+    
+class Receta(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    tiempoPreparacion = models.IntegerField()
+    publica = models.BooleanField()
+    propietario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto)
+
+    def __str__(self):
+        return self.nombre + ' - ' + self.propietario.nombre
+    
+
+class Valoracion(models.Model):
+    id = models.IntegerField(primary_key=True)
+    puntuacion = models.IntegerField()
+    comentario = models.CharField(max_length=200)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.usuario.nombre + ' - ' + self.producto.nombre + ' - ' + str(self.puntuacion)
+    
+class RecetasDesbloqueadasUsuario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    receta = models.ForeignKey(Receta, on_delete=models.CASCADE)
+    disponible = models.BooleanField()
+    fechaBloqueo = models.DateField()
+
+    def __str__(self):
+        return self.usuario.nombre + ' - ' + self.receta.nombre + ' - ' + str(self.disponible)
