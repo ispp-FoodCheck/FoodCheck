f7e6baa2449b440bc1c913346014735315f81daf
Javier Martínez Jaén <javmarjae@gmail.com>
2023-03-05 14:02:05 +0100

    Filtro de alérgenos completo

--- FoodCheck/Web/views.py
+++ FoodCheck/Web/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from .models import Producto
+from .models import Producto, Alergeno
 
 # Create your views here.
 
 def index(request):
-    lista_producto= Producto.objects.all()
-    diccionario={'lista_producto':lista_producto}
+    alergenos_selected = request.GET.getlist('alergenos')
+
+    alergenos = Alergeno.objects.all()
+
+    lista_producto = Producto.objects.exclude(alergenos__nombre__in=alergenos_selected)
+
+    diccionario={'lista_producto':lista_producto,'alergenos_available':alergenos,'alergenos_selected':alergenos_selected}
     return render(request,"products.html",diccionario)
