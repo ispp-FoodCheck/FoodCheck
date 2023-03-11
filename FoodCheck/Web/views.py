0e19aa780e926a170e334c206334c185ee5061b8
josconsan1 <josconsan1@alum.us.es>
2023-03-11 18:30:06 +0100

    #12 : Frontend de la shopping list Hecho fronted completo de la pagina

--- FoodCheck/Web/views.py
+++ FoodCheck/Web/views.py
@@ -58,20 +58,19 @@ def shopping_list(request):
     # Ahora mismo muestra entre 8 productos aleatorios
     productos = set()
     for i in range(10):
-        num_prods = Producto.objects.all().__len__()
+        num_prods = len(Producto.objects.all())
         productos.add(Producto.objects.all()[randint(0, num_prods-1)])
 
     productos_agrupados_por_supermercado = {} #Diccionario que tiene como clave los supermercados y como valor un conjunto de productos que se vendan en ese supermercado
-
+   
     for producto in productos:
         for supermercado in producto.supermercados.all():
-            if productos_agrupados_por_supermercado.keys().__contains__(supermercado):
+            if supermercado in productos_agrupados_por_supermercado.keys():
                 productos_supermercado = productos_agrupados_por_supermercado[supermercado]
                 productos_supermercado.add(producto)
                 productos_agrupados_por_supermercado[supermercado] = productos_supermercado
             else:
                 productos_agrupados_por_supermercado[supermercado] = set([producto])
-
     print(productos_agrupados_por_supermercado)
 
     return render(request,"shopping_list.html", {"productos_agrupados_por_supermercado":productos_agrupados_por_supermercado})
