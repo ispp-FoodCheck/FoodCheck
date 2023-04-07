e82391f261836f545179603f72f40e7bfb6e4acd
José Ignacio <garciaji700@gmail.com>
2023-04-07 11:58:44 +0200

    #151 Base para el testeo

--- FoodCheck/Web/tests.py
+++ FoodCheck/Web/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.db import connection
+from Web.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 
-# Create your tests here.
+class ListaDeLaCompraSeleniumTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        with connection.cursor() as c, open('../datos_iniciales.sql', 'r', encoding='utf-8') as f:
+            c.execute(f.read())
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testing_premium', password='testingpremium', telefono='123456789')
+    
+    def login(self):
+        self.client.force_login(self.user)
